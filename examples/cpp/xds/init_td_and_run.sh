e2d0ba1b26c65ab64a74b29690288f06379cc103
Kannan J <kannanjgithub@google.com>
2024-12-05 12:00:47 +0000

    scratch

--- examples/cpp/xds/init_td_and_run.sh
+++ examples/cpp/xds/init_td_and_run.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+#curl -O https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-cli-linux-x86_64.tar.gz
+#tar -xf google-cloud-cli-linux-x86_64.tar.gz
+#./google-cloud-sdk/install.sh --quiet
+#./google-cloud-sdk/bin/gcloud init
+#send "1"
+#send "1"
+#send "kannanj-psm-interop-testing"
 export GRPC_XDS_BOOTSTRAP=/tmp/td-grpc-bootstrap.json
 # Create the bootstrap file
 curl -L https://storage.googleapis.com/td-grpc-bootstrap/td-grpc-bootstrap-0.18.1.tar.gz | tar -xz
