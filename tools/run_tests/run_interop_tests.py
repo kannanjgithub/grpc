6d967f6b9d349dbd5015b84ce60ee3df8ee4adbf
kannanjgithub <29600796+kannanjgithub@users.noreply.github.com>
2026-02-19 17:36:26 +0000

    Automated change: Fix sanity tests

--- tools/run_tests/run_interop_tests.py
+++ tools/run_tests/run_interop_tests.py
@@ -1825,12 +1825,16 @@ try:
             if "all" in args.language or l in args.language
         )
         if not languages_for_mcs_cs:
-            print('MCS connection scaling tests will be skipped since none of the supported client languages for MCS connection scaling testcases was specified')
-        if args.server != 'java':
-            print('Using java for MCS connection scaling server to be used by all MCS connection scaling clients.')
+            print(
+                "MCS connection scaling tests will be skipped since none of the supported client languages for MCS connection scaling testcases was specified"
+            )
+        if args.server != "java":
+            print(
+                "Using java for MCS connection scaling server to be used by all MCS connection scaling clients."
+            )
         mcs_server_jobspec = server_jobspec(
-            _LANGUAGES['java'],
-            docker_images.get('java'),
+            _LANGUAGES["java"],
+            docker_images.get("java"),
             args.transport_security,
             manual_cmd_log=server_manual_cmd_log,
             set_max_concurrent_streams_limit=True,
@@ -1842,15 +1846,17 @@ try:
         for language in languages_for_mcs_cs:
             test_job = cloud_to_cloud_jobspec(
                 language,
-                'max_concurrent_streams_connection_scaling',
-                'java-mcs',
-                'localhost',
+                "max_concurrent_streams_connection_scaling",
+                "java-mcs",
+                "localhost",
                 mcs_server_job.mapped_port(_DEFAULT_SERVER_PORT),
                 docker_image=docker_images.get(str(language)),
                 transport_security=args.transport_security,
                 manual_cmd_log=client_manual_cmd_log,
-                add_env={'GRPC_EXPERIMENTAL_MAX_CONCURRENT_STREAMS_CONNECTION_SCALING': 'true',
-                    'GRPC_EXPERIMENTS': 'subchannel_connection_scaling'},
+                add_env={
+                    "GRPC_EXPERIMENTAL_MAX_CONCURRENT_STREAMS_CONNECTION_SCALING": "true",
+                    "GRPC_EXPERIMENTS": "subchannel_connection_scaling",
+                },
             )
             jobs.append(test_job)
 
